832dddfb743c484c227b08e5d6f7935000a986db
simeon <simeon>
2012-02-23 13:45:53 +0000

    Add start of grvaity module

--- sbrew/__init__.py
+++ sbrew/__init__.py
@@ -1,6 +1,7 @@
 from quantity import Quantity
 from temp import Temp
 from mass import Mass
+from gravity import Gravity
 from ingredient import Ingredient
 from recipe import Recipe
 from mash import Mash
---
+++ sbrew/gravity.py
@@ -0,0 +1,21 @@
+from quantity import Quantity;
+import numpy
+
+# Data from documentation with True Brue #6800 hydrometer
+# t_data = temps in F
+# c_data = specific gravity corrects to ADD 
+t_data = [50,60,70,77,84,95,105,110,113,118] 
+c_data = [-0.0005,0.0,0.001,0.002,0.003,0.005,0.007,0.008,0.009,0.010]
+
+class Gravity(Quantity):
+
+    def hydrometer_correction(self, temp='60F'):
+        # get specific gravity correction
+        tval = Quantity(temp).to('F')
+        # barf if outside range
+        if (tval<t_data[0] or tval>t_data[-1]):
+	    raise NameError('Temperature out of range for hydrometer, must be within 50 - 118F')
+        correction = numpy.interp(tval,t_data,c_data)
+        sg = self.to('SG') + correction
+        return Quantity(sg,'SG')
+
