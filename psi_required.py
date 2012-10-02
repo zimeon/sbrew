597e87fd4c1353be7a4eec18dc30f8b6f30c64d3
zimeon <simeon.warner@cornell.edu>
2012-10-01 21:32:58 -0400

    Simple calc

---
+++ psi_required.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from sbrew.carbonation import *
+
+t = Quantity(raw_input("Temperature ? "))
+v = Quantity(raw_input("Volumes of CO2 required (float)? "))
+p = psi_required(t,v)
+print "%.1f @ %s requires %s" % (v.value,t,p)
