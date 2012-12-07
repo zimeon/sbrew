b78a7951931cb2ddee03df097b8e611dbe83dfaf
zimeon <simeon.warner@cornell.edu>
2012-12-07 07:14:17 -0500

    Fix parsing of plain value

--- sbrew/test/test_quantity.py
+++ sbrew/test/test_quantity.py
@@ -6,6 +6,13 @@ from sbrew.quantity import Quantity
 
 class TestAll(unittest.TestCase):
 
+    def test_parse(self):
+        self.assertEqual( Quantity("0").value, 0.0 )
+        self.assertEqual( Quantity("2").value, 2.0 )
+        self.assertEqual( Quantity("2.0").value, 2.0 )
+        self.assertEqual( Quantity("2.5").value, 2.5 )
+        self.assertEqual( Quantity("2.000001").value, 2.000001 )
+
     def test_find_conversion(self):
         self.assertAlmostEqual( Quantity.find_conversion('g','kg'), 0.001 )
         self.assertAlmostEqual( Quantity.find_conversion('kg','g'), 1000.0 )
