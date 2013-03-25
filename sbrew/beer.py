245242fc735b81b675bea37507e5acd5ab3ec2de
Simeon Warner <simeon.warner@cornell.edu>
2013-03-24 20:25:29 -0400

    Placeholder for final properties

---
+++ sbrew/beer.py
@@ -0,0 +1,30 @@
+from recipe import Recipe
+from ingredient import Ingredient
+from mass import Mass
+from quantity import Quantity
+from property import Property
+
+class Beer(Recipe):
+    """A placeholder recipe to express final details of beer
+    """
+
+    def __init__(self, subname=None, **kwargs):
+        super(Beer, self).__init__(**kwargs)        
+        self.subname=( subname if subname else 'beer' )
+        if ('start' in kwargs):
+            s = kwargs['start']
+            self.property('OG', s.property('OG'))
+
+    def solve(self):
+        """ Calculate the ABV and attenuation based on OG and FG
+        """
+        pass
+
+    def end_state_str(self):
+        self.solve()
+        s = ''
+        if ('%ABV' in self.properties):
+            s += str(self.property('%ABV').quantity)
+        return(s)
+                                      
+
