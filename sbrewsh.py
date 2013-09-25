315e0f3d72df97b84a4c22393eb363a7be004dfd
Simeon Warner <simeon.warner@cornell.edu>
2013-09-24 23:41:42 -0400

    Add accumulator c

--- sbrewsh.py
+++ sbrewsh.py
@@ -6,21 +6,27 @@
 #
 from sbrew import *
 
-prompt = { 'sb': 'sbsh> ', 'py': '>>>' }
+prompt = { 'sb': 'sbsh> ', 'py': '>>> ' }
 input_mode = 'sb'
 
-def parse_input(i):
-    return(i)
+def parse_input(i, lastval=None):
+    try:
+        c = eval(i, globals(), {'c':lastval} )
+    except Exception as e:
+        print str(e)
+        c = lastval
+    return(c)
 
 # Everything is based around a single recipe, that starts empty
 r = Recipe()
 r.name = 'untitled'
 
 print r
+c = None
 while (1):
     i = raw_input(prompt[input_mode])
-    print "Got " + i
-    c = parse_input(i)
+    c = parse_input(i, lastval=c)
+    print "c = " + str(c)
     if (i == 'q' or i=='quit'):
         break
     elif (i in prompt.keys()):
