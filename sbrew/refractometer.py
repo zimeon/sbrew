09028acaf426e61be6940d2c46c5ca749043057f
Simeon Warner <simeon.warner@cornell.edu>
2013-11-19 01:36:10 -0500

    First stab

--- sbrew/refractometer.py
+++ sbrew/refractometer.py
@@ -1,7 +1,40 @@
 """
+Sources:
+http://www.primetab.com/formulas.html
+http://byo.com/stories/item/1313-refractometers
+
 Online calculators:
 
 http://www.northernbrewer.com/refractometer-calculator/
 """
+from quantity import Quantity
 
+def brix_to_starting_gravity(brix):
+    """Brix reading to starting gravity
 
+    Formula was derived from the 69th edition (1988-1989) of the CRC Handbook of 
+    Chemistry and Physics, "Concentrative Properties of Aqueous Solutions:
+    Conversion Tables", Table 88 Sucrose
+    
+    MathCAD was used to curvefit the data for Degrees Brix @ 20 C (% sucrose 
+    by weight) and specific gravity @ 15 C
+    
+    SG = 1.000898 + 0.003859118*B + 0.00001370735*B*B + 0.00000003742517*B*B*B
+    
+          where:
+
+                   B  = measured refractivity in Brix
+                   SG = calculated specific gravity at 15 C
+    """
+    b = brix.to("Brix")
+    sg = 1.000019 + 0.003865613*b +\
+         0.00001296425*b*b + 0.00000005701128*b*b*b
+    return( Quantity(sg,'sg') )
+
+def starting_gravity_to_brix(sg):
+    """Starting gravity to expected brix reading (plato)
+    
+    Reference: Manning, M.P., Understanding Specific Gravity and Extract, Brewing Techniques, 1,3:30-35 (1993)
+    
+    Plato = -676.67 + 1286.4*SG - 800.47*SG*SG + 190.74*SG*SG*SG
+    """
---
+++ sbrew/test/test_refractometer.py
@@ -0,0 +1,20 @@
+"""Test code for sbrew/refractometer"""
+
+import unittest
+
+from sbrew.quantity import Quantity
+from sbrew.refractometer import brix_to_starting_gravity
+
+class TestAll(unittest.TestCase):
+
+    def test_01_brix_to_starting_gravity(self):
+        self.assertAlmostEqual( brix_to_starting_gravity(Quantity(0,'Brix')).to('sg'), 1.0, places=4 )
+        self.assertAlmostEqual( brix_to_starting_gravity(Quantity(5,'Brix')).to('sg'), 1.020, places=3 )
+        self.assertAlmostEqual( brix_to_starting_gravity(Quantity(10,'Brix')).to('sg'), 1.040, places=3 )
+        self.assertAlmostEqual( brix_to_starting_gravity(Quantity(15,'Brix')).to('sg'), 1.061, places=3 )
+        self.assertAlmostEqual( brix_to_starting_gravity(Quantity(20,'Brix')).to('sg'), 1.083, places=3 )
+
+# If run from command line, do tests
+if __name__ == '__main__':
+    unittest.main()
+
