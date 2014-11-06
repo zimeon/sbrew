1b3311dad783e08bed8bccb7380f1572c60b2123
Simeon Warner <simeon.warner@cornell.edu>
2014-11-06 16:08:15 +0900

    Add for coveralls testing

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+# setuptools used instead of distutils.core so that 
+# dependencies can be handled automatically
+
+setup(
+    name='sbrew',
+    version='0.0.1',
+    packages=['sbrew'],
+    scripts=[],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Operating System :: OS Independent", #is this true? know Linux & OS X ok
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.7"],
+    author='Simeon Warner',
+    author_email='simeon.warner@cornell.edu',
+    description='Simeon\'s Brewing Calculations',
+    long_description=open('README.rst').read(),
+    url='http://github.com/zimeon/sbrew',
+    install_requires=[],
+    test_suite="sbrew/test" #use ugly script to cope with 2 test dirs
+)
