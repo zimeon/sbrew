54b663d854140a6a794b102d0723ac988d63c2c7
Simeon Warner <simeon.warner@cornell.edu>
2014-11-17 22:42:59 -0500

    Tests for sbrew.infusion_mash

--- tests/test_infusion_mash.py
+++ tests/test_infusion_mash.py
@@ -27,7 +27,27 @@ class TestAll(unittest.TestCase):
         self.assertEqual( m2.total_water().value, 0.0 )
 
     def test03_solve(self):
-        pass
+        m1 = InfusionMash()
+        m1.property('temp', '87F' )
+        m1.property('hc_initial', '123Btu/F')
+        m1.property('t_initial', '67F')
+        self.assertRaises( Exception, m1.solve )
+        m1.ingredient('water','strike','4gal')
+        m1.solve()
+        self.assertAlmostEqual( m1.property('t_strike').to('F'), 163.875, places=3 )
+        self.assertAlmostEqual( m1.property('hc_total').to('Btu/F'), 155.0, places=2 )
+        self.assertAlmostEqual( m1.property('total_water').to('gal'), 4.0 )
+        # start from mashtun and grain
+        m2 = InfusionMash()
+        m2.property('temp', '87F' )
+        m2.property('hc_total', '123Btu/F')
+        m2.ingredient('water','strike','4gal')
+        m2.ingredient('grain','marris otter','10lb')
+        m2.solve()
+        self.assertAlmostEqual( m2.property('t_strike').to('F'), 91.1866, places=3 )
+        self.assertAlmostEqual( m2.property('hc_total').to('Btu/F'), 36.962, places=2 )
+        self.assertAlmostEqual( m2.property('total_water').to('gal'), 4.0 )
+
 
 # If run from command line, do tests
 if __name__ == '__main__':
