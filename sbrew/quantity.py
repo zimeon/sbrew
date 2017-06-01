78cdd17bc37f28cfbccf5ed95c61799695115b2c
Simeon Warner <simeon.warner@cornell.edu>
2017-05-31 22:33:43 -0400

    Do cyser calc

--- sbrew/quantity.py
+++ sbrew/quantity.py
@@ -15,52 +15,55 @@ class Quantity:
     Qantities have a value and a unit. Without a unit a quantity is dimensionless.
     """
 
-    CONVERSIONS = { 
-        '%ABW' : { '%ABV' : 1.25 },
-        'oz'  : { 'lb' : 1.0/16.0,
-                  'kg' : 0.0283495231,
-                  'g'  : 28.3495231 },
-        'gal' : { 'pt' : 8.0,
-                  'qt' : 4.0,
-                  'l'  : 3.78541,
-                  'dl' : 37.8541,
-                  'cl' : 378.541,
-                  'ml' : 3785.41 },
-        'J'   : { 'Joule' : 1.0,
-                  'kJ' : 0.001,
-                  'Btu' : 0.0009478 },
-        'points' : { },
-        'Btu/lb/F' : { 'kJ/kg/F' : 2.324444 },
-        'h'   : { 'min' : 60,
-                  's'   : 3600 },
-        'fraction' : { '%': 100.0 },
-        }
+    CONVERSIONS = {
+        '%ABW': {'%ABV': 1.25},
+        'oz': {'lb': 1.0 / 16.0,
+               'kg': 0.0283495231,
+               'g': 28.3495231},
+        'gal': {'pt': 8.0,
+                'qt': 4.0,
+                'l': 3.78541,
+                'dl': 37.8541,
+                'cl': 378.541,
+                'ml': 3785.41},
+        'J': {'Joule': 1.0,
+              'kJ': 0.001,
+              'Btu': 0.0009478},
+        'points': {},
+        'Btu/lb/F': {'kJ/kg/F': 2.324444},
+        'h': {'min': 60,
+              's': 3600},
+        'fraction': {'%': 100.0},
+    }
 
     DISPLAY_FMT = {
-        '%ABV' : '%.1f',
-        '%ABW' : '%.1f',
-        '%atten' : '%.1f',
-        'Btu/F' : '%.2f',
-        'F' : '%.1f',
-        'gal' : '%.2f',
-        'psi' : '%.1f',
-        'points' : '%.1f',
-        'min' : '%d',
-        'og' : '%.3f',
-        'sg' : '%.3f',
-        'oz' : '%.2f',
-        'IBU' : '%.1f',
-        'MCU' : '%.2f',
-        'SRM' : '%.1f',
-        'fraction' : '%.3f',
-        '%' : '%.1f',
-        }
+        '%ABV': '%.1f',
+        '%ABW': '%.1f',
+        '%atten': '%.1f',
+        'Btu/F': '%.2f',
+        'F': '%.1f',
+        'gal': '%.2f',
+        'psi': '%.1f',
+        'points': '%.1f',
+        'min': '%d',
+        'og': '%.3f',
+        'sg': '%.3f',
+        'oz': '%.2f',
+        'IBU': '%.1f',
+        'MCU': '%.2f',
+        'SRM': '%.1f',
+        'fraction': '%.3f',
+        '%': '%.1f',
+    }
 
+    # 'non-canonical': 'canonical'
     CANONICAL_UNIT = {
         'gal/hour': 'gal/h',
-        'hour'    : 'h',
+        'hour': 'h',
         'dimensionless': None,
-        }
+        'SG': 'sg',
+        'OG': 'og'
+    }
 
     ALL_CONV = None
 
@@ -83,10 +86,11 @@ class Quantity:
             self.unit = value.unit
         elif (unit is None):
             # try to parse value and unit from string
-            m = re.match('\s*([-+]?\d+(\.\d*)?|\?)\s*([A-Za-z%][\w/%]*)\s*$', value)
+            m = re.match(
+                '\s*([-+]?\d+(\.\d*)?|\?)\s*([A-Za-z%][\w/%]*)\s*$', value)
             if (m):
-                self.value=None if (m.group(1)=='?') else float(m.group(1))
-                self.unit=m.group(3)
+                self.value = None if (m.group(1) == '?') else float(m.group(1))
+                self.unit = m.group(3)
             else:
                 self.value = float(value)
                 self.unit = unit
@@ -101,7 +105,7 @@ class Quantity:
         elif (self.unit is None):
             return(str(self.value) + " (dimensionless)")
         elif (self.unit in Quantity.DISPLAY_FMT):
-            return( (Quantity.DISPLAY_FMT[self.unit] % self.value) + " " + str(self.unit))
+            return((Quantity.DISPLAY_FMT[self.unit] % self.value) + " " + str(self.unit))
         else:
             return(str(self.value) + " " + str(self.unit))
 
@@ -121,7 +125,7 @@ class Quantity:
             return Quantity.CANONICAL_UNIT[unit]
         return unit
 
-    def to(self,new_unit):
+    def to(self, new_unit):
         """Get value of this quantity in a specific unit.
 
         Uses conversions in CONVERSIONS dictionary of dictionaries
@@ -130,118 +134,127 @@ class Quantity:
         add.
 
         e.g. lbs = weight.to('lb')
-             temp=Quantity('10C')
+             temp = Quantity('10C')
              f = temp.to('F')
         """
-        # Make sure new_unit is canonical
-        new_unit=self.canonical_unit(new_unit)
+        # Make sure current and new units are canonical
+        self.unit = self.canonical_unit(self.unit)
+        new_unit = self.canonical_unit(new_unit)
         # Convert
-        if (self.unit==new_unit):
+        if (self.unit == new_unit):
             return(self.value)
 
         elif (self.unit in Quantity.CONVERSIONS and
               new_unit in Quantity.CONVERSIONS[self.unit]):
-            return(self.value*Quantity.CONVERSIONS[self.unit][new_unit])
+            return(self.value * Quantity.CONVERSIONS[self.unit][new_unit])
         elif (new_unit in Quantity.CONVERSIONS and
               self.unit in Quantity.CONVERSIONS[new_unit]):
-            #inverse
-            return(self.value/Quantity.CONVERSIONS[new_unit][self.unit])
-        elif (self.unit in ['C','F'] and new_unit in ['C','F']):
+            # inverse
+            return(self.value / Quantity.CONVERSIONS[new_unit][self.unit])
+        elif (self.unit in ['C', 'F'] and new_unit in ['C', 'F']):
             return(self.temp_to(new_unit))
         else:
-            return(self.value * self.find_conversion(self.unit,new_unit))
+            return(self.value * self.find_conversion(self.unit, new_unit))
 
     def temp_to(self, new_unit):
         """Temperature conversion method."""
         if (self.unit == 'F' and new_unit == 'C'):
-            return((self.value-32.0)*5.0/9.0)
+            return((self.value - 32.0) * 5.0 / 9.0)
         elif (self.unit == 'C' and new_unit == 'F'):
-            return((self.value*9.0/5.0)+32.0)
+            return((self.value * 9.0 / 5.0) + 32.0)
         else:
             raise ConversionError("unknown temperature conversion")
 
-    def convert_to(self,new_unit):
+    def convert_to(self, new_unit):
         """Convert internal value to the new unit.
 
         Piggbybacks on the to() method but changes the internal
         representation. Returns self.
         """
         # Make sure new_unit is canonical
-        new_unit=self.canonical_unit(new_unit)
+        new_unit = self.canonical_unit(new_unit)
         self.value = self.to(new_unit)
         self.unit = new_unit
         return(self)
 
-    def __add__(self,other):
+    def __add__(self, other):
         """Add two quantities, return result in units of first."""
-        sum=self.value+other.to(self.unit)
-        return(Quantity(sum,self.unit))
+        sum = self.value + other.to(self.unit)
+        return(Quantity(sum, self.unit))
 
-    def __sub__(self,other):
+    def __sub__(self, other):
         """Subtract one quantity from another, return result in units of first."""
-        sum=self.value-other.to(self.unit)
-        return(Quantity(sum,self.unit))
+        sum = self.value - other.to(self.unit)
+        return(Quantity(sum, self.unit))
 
-    def __mul__(self,frac):
+    def __mul__(self, frac):
         """Mulitply quantity by a fraction, return new quantity as result."""
-        return(Quantity(self.value*frac,self.unit))
+        return(Quantity(self.value * frac, self.unit))
 
     __rmul__ = __mul__
 
     @staticmethod
-    def find_conversion(from_unit,to_unit):
+    def find_conversion(from_unit, to_unit):
         """Function to see whether we can fo from_unit->to_unit.
 
         Raises and exception if not, otherwise returns factor that the value
         in from_unit is multiplied by to get a value in to_unit
         """
-        if (from_unit==to_unit):
-            return(1.0);
+        if (from_unit == to_unit):
+            return(1.0)
         if (Quantity.ALL_CONV is None):
             Quantity._build_ALL_CONV()
         if (not (from_unit in Quantity.ALL_CONV)):
-            raise ConversionError('unknown original unit in conversion requested from %s to %s' % (from_unit, to_unit))
+            raise ConversionError(
+                'unknown original unit in conversion requested from %s to %s' % (from_unit, to_unit))
         if (not (to_unit in Quantity.ALL_CONV)):
-            raise ConversionError('unknown destination unit in conversion requested from %s to %s' % (from_unit,to_unit))
+            raise ConversionError(
+                'unknown destination unit in conversion requested from %s to %s' % (from_unit, to_unit))
         if (not (to_unit in Quantity.ALL_CONV[from_unit])):
-            raise ConversionError('unknown conversion requested from %s to %s' % (from_unit,to_unit))
+            raise ConversionError(
+                'unknown conversion requested from %s to %s' % (from_unit, to_unit))
         return(Quantity.ALL_CONV[from_unit][to_unit])
 
     @staticmethod
     def _build_ALL_CONV():
-        # Expand tree of all CONVERSIONS (include sanity check to 
+        # Expand tree of all CONVERSIONS (include sanity check to
         # avoid possible cycles)
         #
-        # Build local self.conv with data from Quantity.CONVERSIONS and inverses
-        Quantity.ALL_CONV={}
+        # Build local self.conv with data from Quantity.CONVERSIONS and
+        # inverses
+        Quantity.ALL_CONV = {}
         for f in Quantity.CONVERSIONS:
-            Quantity.ALL_CONV[f]={}
+            Quantity.ALL_CONV[f] = {}
             for t in Quantity.CONVERSIONS[f]:
-                Quantity.ALL_CONV[f][t]=Quantity.CONVERSIONS[f][t]
+                Quantity.ALL_CONV[f][t] = Quantity.CONVERSIONS[f][t]
                 if (not (t in Quantity.CONVERSIONS)):
-                    Quantity.ALL_CONV[t]={}
+                    Quantity.ALL_CONV[t] = {}
                 if (not (f in Quantity.ALL_CONV[t])):
-                    Quantity.ALL_CONV[t][f]= 1.0 / Quantity.ALL_CONV[f][t] #inverse
+                    Quantity.ALL_CONV[t][f] = 1.0 / \
+                        Quantity.ALL_CONV[f][t]  # inverse
         # Now expand tree by repeatedly adding two-step paths as one
         for s in list(Quantity.ALL_CONV.keys()):
             for m in list(Quantity.ALL_CONV[s].keys()):
                 for e in Quantity.ALL_CONV[m]:
                     if (not (e in Quantity.ALL_CONV[s])):
-                        Quantity.ALL_CONV[s][e] = Quantity.ALL_CONV[s][m] * Quantity.ALL_CONV[m][e]
+                        Quantity.ALL_CONV[s][e] = Quantity.ALL_CONV[
+                            s][m] * Quantity.ALL_CONV[m][e]
                         Quantity.ALL_CONV[e][s] = 1.0 / Quantity.ALL_CONV[s][e]
 
     # Data from documentation with True Brue #6800 hydrometer
     # t_data = temps in F
-    # c_data = specific gravity corrects to ADD 
-    t_data = [50,60,70,77,84,95,105,110,113,118] 
-    c_data = [-0.0005,0.0,0.001,0.002,0.003,0.005,0.007,0.008,0.009,0.010]
+    # c_data = specific gravity corrects to ADD
+    t_data = [50, 60, 70, 77, 84, 95, 105, 110, 113, 118]
+    c_data = [-0.0005, 0.0, 0.001, 0.002, 0.003,
+              0.005, 0.007, 0.008, 0.009, 0.010]
 
     def hydrometer_correction(self, temp='60F'):
         """Return specific gravity correction at given temperature."""
         tval = Quantity(temp).to('F')
         # barf if outside range
-        if (tval<self.t_data[0] or tval>self.t_data[-1]):
-           raise NameError('Temperature out of range for hydrometer, must be within 50 - 118F')
-        correction = numpy.interp(tval,self.t_data,self.c_data)
+        if (tval < self.t_data[0] or tval > self.t_data[-1]):
+            raise NameError(
+                'Temperature out of range for hydrometer, must be within 50 - 118F')
+        correction = numpy.interp(tval, self.t_data, self.c_data)
         sg = self.to('sg') + correction
-        return Quantity(sg,'sg')
+        return Quantity(sg, 'sg')
--- sbrew/refractometer.py
+++ sbrew/refractometer.py
@@ -20,23 +20,25 @@ def brix_to_starting_gravity(brix):
     MathCAD was used to curvefit the data for Degrees Brix @ 20 C (% sucrose 
     by weight) and specific gravity @ 15 C
     
-    sg = 1.000898 + 0.003859118*B + 0.00001370735*B*B + 0.00000003742517*B*B*B
+        sg = 1.000898 + 0.003859118*B + 0.00001370735*B*B + 0.00000003742517*B*B*B
     
-          where:
+    where:
 
-                   B  = measured refractivity in Brix
-                   sg = calculated specific gravity at 15 C
+        B  = measured refractivity in Brix
+        sg = calculated specific gravity at 15 C
     """
     b = brix.to("Brix")
     sg = 1.000019 + 0.003865613*b +\
          0.00001296425*b*b + 0.00000005701128*b*b*b
-    return( Quantity(sg,'sg') )
+    return(Quantity(sg,'sg'))
 
-def starting_gravity_to_brix(sg):
+def starting_gravity_to_brix(starting_gravity):
     """Starting gravity to expected brix reading (plato).
     
     Reference: Manning, M.P., Understanding Specific Gravity and Extract, Brewing Techniques, 1,3:30-35 (1993)
     
-    Plato = -676.67 + 1286.4*sg - 800.47*sg*sg + 190.74*sg*sg*sg
+        Plato = -676.67 + 1286.4*sg - 800.47*sg*sg + 190.74*sg*sg*sg
     """
-    raise(Exception("IMPLEMENT ME - FIXME"))
+    sg = starting_gravity.to('SG')
+    b = -676.67 + 1286.4*sg - 800.47*sg*sg + 190.74*sg*sg*sg
+    return(Quantity(b, 'Brix'))
