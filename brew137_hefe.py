5169d98640afdca634cc3392429f853d51ad7650
Simeon Warner <simeon.warner@cornell.edu>
2020-08-05 17:41:18 -0400

    Hefe

---
+++ brew137_hefe.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from sbrew import *
+
+r = Recipe("Warner Weisse")
+
+m = DecoctionMash()
+m.ingredient('grain', '2-row (GM)', '4lb')
+m.ingredient('grain', 'wheat malt (Weyermann Pale)', '6lb')
+m.total_grains(Quantity('100lb'))
+m.add_step('infuse', volume='3.6gal', temp='108F')
+m.add_step('heat', temp='122F', time='10min')
+m.add_step('rest', time='15min')
+d1 = m.split(time='5min', remove='40%')
+d1.add_step('heat', temp='160F', time='15min')
+d1.add_step('rest', time='15min')
+d1.add_step('heat', temp='212F', time='15min')
+d1.add_step('boil', time='20min')
+m.mix(decoction=d1, time='10min')
+m.add_step('adjust', temp='147F')
+m.add_step('rest', time='20min')
+m.add_step('heat', temp='160F', time='7min')
+m.add_step('rest', time='13min')
+m.add_step('infuse', temp='170F', infusion_temp='212F',
+           volume='1.2gal', time='5min')
+r.add(m)
+
+s = BatchSparge(start=m)
+s.property('wort_volume', '6.75gal')
+r.add(s)
+
+b = Boil(start=s, duration="60min")
+b.ingredient('hops', 'hallertau', '12IBU', time='60min', aa='3.7%AA')
+b.ingredient('hops', 'hallertau', '0.25oz', time='15min', aa='4.0%AA')
+b.property('boil_end_volume', '6.25gal')
+r.add(b)
+
+f = Ferment(start=b)
+f.ingredient('yeast', 'wyeast 3056 bavarian weizen', '1packet')
+f.property('atten', '80.0%atten')
+r.add(f)
+
+r.solve()
+print(r)
